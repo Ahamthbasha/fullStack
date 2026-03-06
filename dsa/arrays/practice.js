7a2ad8efb0eebad9171cd2872269e1ed6796736e
Ahamathbasha <bashachristopherallah786@gmail.com>
2026-03-06 13:16:17 +0530

    array dsa relearning

--- dsa/arrays/practice.js
+++ dsa/arrays/practice.js
@@ -64,3 +64,315 @@
 // }
 
 // console.log(-1)
+
+//5.reverse an array
+
+// let arr = [2,4,6,8,10]
+
+// let start = 0
+// let end = arr.length-1
+
+// while(start <= end){
+//     let temp = arr[start]
+//     arr[start] = arr[end]
+//     arr[end] = temp
+//     start++
+//     end--
+// }
+
+// console.log(arr)
+
+//6.pairs an array
+
+// let arr = [2,4,6,8,10]
+
+// for(let i=0;i<arr.length;i++){
+//     let str = ''
+//     for(let j=i+1;j<arr.length;j++){
+//         str += `(${arr[i]},${arr[j]})` + ' '
+//     }
+//     console.log(str)
+// }
+
+//7.print subarray
+
+// let arr = [2,4,6,8,10]
+
+// let result = []
+
+// for(let i=0;i<arr.length;i++){
+//     result.push([arr[i]])
+//     let currentIndex = i
+//     while(currentIndex != arr.length-1){
+//         let temp = []
+//         for(let j=i;j<=currentIndex+1;j++){
+//             temp.push(arr[j])
+//         }
+//         result.push(temp)
+//         currentIndex++
+//     }    
+// }
+// console.log(result)
+
+//alternate way
+// let result = []
+
+// for(let i=0;i<arr.length;i++){
+//     let start = i
+
+//     for(let j=i;j<arr.length;j++){
+//         let end = j
+//         let temp = []
+//         for(let k=start;k<=end;k++){
+//             temp.push(arr[k])
+//         }
+//         result.push(temp)
+//     }
+// }
+
+// console.log(result)
+
+// 8. max subarray sum
+
+// let arr = [1,-2,6,-1,3]
+
+// let max = -Infinity
+// let subArray = []
+
+// for(let i=0;i<arr.length;i++){
+//     let start = i
+//     for(let j=i;j<arr.length;j++){
+//         let end = j
+//         let temp = []
+//         for(let k=start;k<=end;k++){
+//             temp.push(arr[k])
+//         }
+
+//         let sum = 0
+
+//         for(let val of temp){
+//             sum += val
+//         }
+
+//         if(sum > max){
+//             max = sum
+//             subArray = temp
+//         }
+//     }
+// }
+
+// console.log(max,subArray)
+
+//maximum Subarray sum 
+
+// let maxSum = -Infinity
+
+// for(let i=0;i<arr.length;i++){
+//     let start = i
+//     for(let j=i;j<arr.length;j++){
+//         let end = j
+//         let currentSum = 0
+
+//         for(let k=start;k<=end;k++){
+//             currentSum += arr[k]
+//         }
+
+//         if(currentSum > maxSum){
+//             maxSum = currentSum
+//         }
+//     }
+// }
+
+// console.log(maxSum)
+
+//max subarray sum alternate way(prefix sum)
+
+// let arr = [1,-2,6,-1,3]
+
+
+// let prefix = []
+
+// for(let i=0;i<arr.length;i++){
+//     if(i == 0){
+//         prefix.push(arr[i])
+//     }else{
+//         prefix.push(prefix[prefix.length-1] + arr[i])
+//     }
+// }
+
+// let maxSum = -Infinity
+
+// for(let i=0;i<arr.length;i++){
+//     let start = i
+//     for(let j = i;j<arr.length;j++){
+//         let end = j
+//         let currentSum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1]
+//         if(currentSum > maxSum){
+//             maxSum = currentSum
+//         }
+//     }
+// }
+
+// console.log(maxSum)
+
+// kadane's algo
+
+// let arr = [-2,-3,4,-1,-2,1,5,-3]
+
+// let arr = [1,-2,6,-1,3]
+
+// let currentSum = 0
+// let max = -Infinity
+
+// for(let i=0;i<arr.length;i++){
+//         currentSum += arr[i]
+//         if(currentSum < 0){
+//             currentSum = 0
+//         }
+//         max = Math.max(currentSum,max)
+// }
+
+// console.log(max)
+
+//9.Trapping rain water problem
+
+// let height = [4,2,0,6,3,2,5]
+
+// if(height.length < 2){
+//     console.log(false)
+// }
+
+// let leftMaxBoundary = []
+
+// for(let i=0;i<height.length;i++){
+//     if(i == 0){
+//         leftMaxBoundary[i]=height[i]
+//     }else{
+//         leftMaxBoundary[i] = (Math.max(leftMaxBoundary[i-1],height[i]))
+//     }
+// }
+
+// let rightMaxBoundary = []
+
+// for(let i=height.length-1;i>=0;i--){
+//     if(i == height.length-1){
+//         rightMaxBoundary[i] = height[i]
+//     }else{
+//         rightMaxBoundary[i] = Math.max(rightMaxBoundary[i+1],height[i])
+//     }
+// }
+
+// let trappedRainWater = 0
+
+// for(let i=0;i<height.length;i++){
+//     trappedRainWater += (Math.min(leftMaxBoundary[i],rightMaxBoundary[i]) - height[i])
+// }
+
+// console.log(trappedRainWater)
+
+//10.buy and sell stocks
+
+// let prices = [7,1,5,3,6,4]
+
+// let buyPrice = Infinity
+// let maxProfit = 0
+// for(let val of prices){
+//     if(buyPrice < val){
+//         let profit = Math.abs(buyPrice - val)
+//         maxProfit = Math.max(profit,maxProfit)
+//     }else{
+//         buyPrice = val
+//     }
+// }
+
+// console.log(maxProfit)
+
+//12.given an integer array nums,return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// let nums = [1,1,1,3,3,4,3,2,4,2]
+// let nums = [1,2,3,4]
+// let nums = [1,2,3,1]
+
+//my logic
+
+// let freqMap = {}
+
+// for(let val of nums){
+//     if(freqMap[val]){
+//         freqMap[val]++
+//     }else{
+//         freqMap[val]=1
+//     }
+// }
+
+// for(let key in freqMap){
+//     if(freqMap[key] == 2){
+//         console.log(true)
+//         return
+//     }
+// }
+
+// console.log(false)
+
+// using set
+
+// let set = new Set()
+
+// for(let val of nums){
+//     if(set.has(val)){
+//         console.log(true)
+//         return
+//     }else{
+//         set.add(val)
+//     }
+// }
+
+// console.log(false)
+
+//13.Return the index of the target element in the array otherwise return false
+
+// my logic
+
+// let nums = [4,5,6,7,0,1,2]
+// let target = 1
+
+// for(let i=0;i<nums.length;i++){
+//     if(nums[i] == target){
+//         console.log(i)
+//         return
+//     }
+// }
+// console.log(-1)
+
+//we need to solve it using O(logn) algorithm
+
+// let left = 0
+// let right = nums.length-1
+
+
+// while(left <= right){
+//     let mid = Math.floor((left+right)/2)
+
+//     if(nums[mid] == target){
+//         console.log(mid)
+//         return
+//     }
+
+//     if(nums[left] <= nums[mid]){
+//         if(target > nums[left] && target <= nums[mid]){
+//             right = mid-1
+//         }else{
+//             left = mid+1
+//         }
+//     }else{
+//         if(target > nums[right] && target <= nums[mid]){
+//             left = mid+1
+//         }
+//         else{
+//             right = mid-1
+//         }
+//     }
+// }
+
+// console.log(-1)
+
