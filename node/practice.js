43115feb8a2a1b56dad69edf8588b1afe877b565
Ahamathbasha <bashachristopherallah786@gmail.com>
2026-06-17 19:12:22 +0530

    learned about terminal commands

--- node/practice.js
+++
@@ -1,8 +0,0 @@
-// node environment document and window wont be available. It available only in the browser
-
-// console.log(document)
-// console.log(window)
-
-// node environment has global
-
-console.log(global)
